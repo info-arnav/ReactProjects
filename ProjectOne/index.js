0aebc39778d78324053f75d89652b44a5f2528e3
info-arnav <60637717+info-arnav@users.noreply.github.com>
2020-05-03 18:07:16 +0530

    uodate

---
+++ ProjectOne/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { render } from 'react-dom'
+import { useScroller } from './useScroller'
+var app = () => {
+    var [Tester] = useScroller('testClass', 'testInput', '-', [1,2,3,4,5,6,7,8,9,10])
+    return (
+        <div>
+            <h1></h1>
+            <Tester />
+        </div>
+    )
+}
+render(
+    React.createElement(app),
+    document.getElementById("root")
+  );
